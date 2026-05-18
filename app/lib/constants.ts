7ef43c4af731111cd2f496e0550bffca1770e7ba
tsuppin <dt7210966@gmail.com>
2026-05-18 11:13:36 +0900

    feat(ai): auto-add learning patch v113.1

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -4637,4 +4637,51 @@ export const INITIAL_PATCHES: LearningPatch[] = [
     ],
     active: true
   },
+  {
+    id: "patch_1779070415440",
+    version: "v113.1",
+    date: "2026-05-18T02:13:35.440Z",
+    description: "東京 - 好走馬(ドンエレクトス等)の特性学習",
+    track: "東京",
+    condition: "良",
+    adjustments: [
+      {
+        "field": "age",
+        "operator": "==",
+        "value": 3,
+        "scoreAdjust": 15
+      },
+      {
+        "field": "jockey",
+        "operator": "includes",
+        "value": "荻野 極",
+        "scoreAdjust": 15
+      },
+      {
+        "field": "sire",
+        "operator": "includes",
+        "value": "ダノンレジェンド",
+        "scoreAdjust": 15
+      },
+      {
+        "field": "frame",
+        "operator": "<=",
+        "value": 2,
+        "scoreAdjust": 15
+      },
+      {
+        "field": "jockey",
+        "operator": "includes",
+        "value": "C.ルメール",
+        "scoreAdjust": 15
+      },
+      {
+        "field": "sire",
+        "operator": "includes",
+        "value": "ロードカナロア",
+        "scoreAdjust": 15
+      }
+    ],
+    active: true
+  },
 ];
