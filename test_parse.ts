2c4137c1ff965f0a79fa7a9969f55c3630ecea43
tsuppin <dt7210966@gmail.com>
2026-06-19 08:55:29 +0900

    Remove corrupted test scripts

--- test_parse.ts
+++
@@ -1,8 +0,0 @@
-import { parseNARText } from "./app/lib/parser";
-
-const text = `
-1 1 繝ｪ繧ｾ繝ｼ繝医い繧､繝ｩ繝ｳ繝・迚｡3 57.0 譴・伐 譎ｺ蜿ｸ
-2 2 繝槭Ν繧ｬ繧､繧ｳ繧ｦ繝・う 迚｡3 57.0 (譬ｪ)繧｢繧､繝・3 3 33F 迚｡4 55.0
-`;
-
-console.log(JSON.stringify(parseNARText(text).horses, null, 2));
